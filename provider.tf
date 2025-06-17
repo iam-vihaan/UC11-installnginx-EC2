498b3270edca9d0b7e9930cacfeb4bcbd8ab36ec
iam-vihaan <asathyavihaan@gmail.com>
2025-06-17 14:30:28 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,11 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    random = {
-      source = "hashicorp/random"
-      version = "~> 3.0"
- }
-
   }
-  required_version = ">= 1.10.0"
+  required_version = ">= 1.6.0"
+}
+
+provider "aws" {
+  region = var.aws_region
 }
